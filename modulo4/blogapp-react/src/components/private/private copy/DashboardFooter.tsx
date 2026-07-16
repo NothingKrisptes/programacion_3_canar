997ea940f6ef9d571e5388197c26a0ee111fbc8e
nothingkrisptes <iancm0168@gmail.com>
2026-07-16 08:56:47 -0500

    clase 16/07/2026

--- modulo4/blogapp-react/src/components/private/private copy/DashboardFooter.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/private/DashboardFooter.tsx
-export default function DashboardFooter() {
-  return (
-    <footer className="border-t p-4 text-center text-sm text-muted-foreground">
-      Panel de administración — BlogApp
-    </footer>
-  )
-}
--- modulo4/blogapp-react/src/pages/private/components/public/PublicFooter.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/public/PublicFooter.tsx
-export default function PublicFooter() {
-  return (
-    <footer className="border-t p-4 text-center text-sm text-muted-foreground">
-      © {new Date().getFullYear()} BlogApp Cañar
-    </footer>
-  )
-}
